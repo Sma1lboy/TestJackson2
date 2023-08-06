3b4d0d73b13419a99afe42b770eee4eed09e6fca
Sma1lboy <541898146chen@gmail.com>
2023-08-06 15:12:20 +0800

    first init

---
+++ src/main/java/com/jackson/testjackson2/TestJackson2Application.java
@@ -0,0 +1,13 @@
+package com.jackson.testjackson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJackson2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJackson2Application.class, args);
+    }
+
+}
